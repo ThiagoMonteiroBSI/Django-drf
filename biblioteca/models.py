b544a074f1ed1f7447ce8db33c4ff4205eae934c
ThiagoMonteiroBSI <thiagodepaulamonteiro@gmail.com>
2026-03-10 19:12:38 -0300

    feat: criacao de nova model

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -38,7 +38,7 @@ class Livro(models.Model):
     editora = models.ForeignKey(
         Editora, on_delete=models.PROTECT, related_name="livros"
     )
-    autor = models.ManyToManyField(Autor, related_name="livros")
+    autor = models.ManyToManyField(Autor, related_name="livros  ")
     
     def __str__(self):
-        return f"{self.titulo} ({self.quantidade})"
+        return f"{self.titulo} ({self.quantidade})"#commit qualquer
